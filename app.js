bd97ded474d51373b45af9758395ce23f969aa16
Shiv91996 <sanjog.mard69@gmail.com>
2021-06-14 21:54:58 +0530

    added Logic for Calculator

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+var evalStr = "";
 const clickHandler = (event) => {
-  console.log(event);
-  if (event.target.classList[1] === "digit") console.log("number");
+  var display = document.querySelector("#display");
+  console.log(event.target.innerHTML);
+  if (event.target.innerHTML === "=") {
+    try {
+      display.value = eval(evalStr);
+      evalStr = "";
+    } catch (e) {}
+  } else {
+    evalStr = evalStr + event.target.innerHTML;
+    display.value = evalStr;
+  }
+  console.log(evalStr);
 };
 
 document.querySelector("#calculator").addEventListener("click", clickHandler);
